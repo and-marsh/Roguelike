8c8c29efc7aa023ba3d003d4f15e100aa1ce758d
Andrew Marshalov <and.marsh@yandex.ru>
2018-11-11 15:13:21 +0300

    new logic for creating simple gungeon

--- Part1/Map.h
+++ Part1/Map.h
@@ -4,7 +4,7 @@
 struct Tile
 {
 	bool canWalk; // can we walk through this tile?
-	Tile() : canWalk(true) {}
+	Tile() : canWalk(false) {}
 };
 
 class Map
@@ -18,6 +18,19 @@ public:
 	void render() const;
 protected:
 	Tile * tiles;
+	friend class BspListener;
 
-	void setWall(int x, int y);
+	void dig(int x1, int y1, int x2, int y2);
+	void createRoom(bool first, int x1, int y1, int x2, int y2);
+};
+
+class BspListener : public ITCODBspCallback 
+{
+private:
+	Map & map; // a map to dig
+	int roomNum; // room number
+	int lastx, lasty; // center of the last room
+public:
+	BspListener(Map &map);
+	bool visitNode(TCODBsp *node, void *userData);
 };
