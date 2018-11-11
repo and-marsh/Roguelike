c01695bbe1e1d01f528e93500c5e00ae84341f6d
Andrew Marshalov <and.marsh@yandex.ru>
2018-11-11 13:04:24 +0300

    add Map class

---
+++ Part1/Map.cpp
@@ -0,0 +1,33 @@
+#include "Map.h"
+
+#include "libtcod.hpp"
+#include <vector>
+
+Map::Map(int width, int height) :
+	width(width),
+	height(height)
+{
+	tiles.reserve(width*height);
+	setWall(30, 22);
+	setWall(50, 22);
+}
+
+bool Map::isWall(int x, int y) const
+{
+	return !tiles[x + y * width].canWalk;
+}
+
+void Map::render() const
+{
+	static const TCODColor darkWall(0, 0, 100);
+	static const TCODColor darkGround(50, 50, 150);
+	for (int x = 0; x < width; x++)
+		for (int y = 0; y < height; y++) 
+			TCODConsole::root->setCharBackground(x, y, isWall(x, y) ? darkWall : darkGround);
+}
+
+void Map::setWall(int x, int y)
+{
+	tiles[x + y * width].canWalk = false;
+}
+
